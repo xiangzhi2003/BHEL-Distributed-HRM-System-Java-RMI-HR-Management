a1ffc391a766471f19371d14dceedbe9b4cbce00
CHIANG XIANG ZHI <xiangzhichiang2003@gmail.com>
2026-01-20 01:03:18 +0800

    Create RMI remote interface

---
+++ src/main/java/server/AuthInterface.java
@@ -0,0 +1,9 @@
+package server;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface AuthInterface extends Remote {
+    // Login
+    String login(String email, String password) throws RemoteException;
+}
